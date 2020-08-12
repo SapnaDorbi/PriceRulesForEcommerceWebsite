a32788d395f3b5608fdade62fa0d9041a26342eb
SapnaDorbi <savydorbi@gmail.com>
2020-08-13 01:13:51 +0530

    complete deletion functionality of saved price rules from UI

--- js/storage.js
+++ js/storage.js
@@ -34,26 +34,39 @@ class UI {
   static displaySavePriceRules() {
     const savedPriceRules = Store.getPriceRules();
     if (savedPriceRules.length > 0) {
-      Object.values(savedPriceRules[0]).forEach((savedPriceRule) => {
-        UI.addPriceRuleToList(savedPriceRule);
+      Object.values(savedPriceRules[0]).forEach((savedPriceRule, key) => {
+        UI.addPriceRuleToList(savedPriceRule, key + 1);
       });
     }
   }
 
-  static addPriceRuleToList(savedPriceRule, index) {
+  static addPriceRuleToList(savedPriceRule, key) {
     const price_rule = document.querySelector("#saved-price-rules");
     const row = document.createElement("tr");
 
-    row.innerHTML = `<td>${savedPriceRule.min_price}</td>
+    row.innerHTML = `<td>${key}</td>
+      <td>${savedPriceRule.min_price}</td>
       <td>${savedPriceRule.max_price}</td>
       <td>${savedPriceRule.price_action}</td>
       <td>${savedPriceRule.apply_as}</td>
       <td>${savedPriceRule.adjustment_amount}</td>
-      <td><a href="#" class="error btn-sm delete">X</a></td>`;
+      <td><a href="#" class="delete-row btn btn-danger">X</a></td>`;
 
     price_rule.append(row);
+  }
+
+  static deletePriceRule(el) {
+    if (el.classList.contains("delete-row")) {
+      el.parentNode.parentNode.remove();
+    }
   }
 }
 
 //Event: Display saved price rules
 document.addEventListener("DOMContentLoaded", UI.displaySavePriceRules);
+
+//Event: Remove a saved price rule
+document.querySelector("#saved-price-rules").addEventListener("click", (e) => {
+  //Remove a saved price rule from UI
+  UI.deletePriceRule(e.target);
+});
